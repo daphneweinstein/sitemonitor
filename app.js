3934b6a435ce233af05df5a91a0b62d76cafc67b
daphneweinstein <dhaw@princeton.edu>
2015-07-22 13:32:27 -0400

    working with no historical data--just single page

--- app.js
+++ app.js
@@ -2,15 +2,21 @@ var config = require('./config.js');
 var request = require('request');
 var async = require('async');
 var express = require('express');
+var nunjucks = require('nunjucks');
 
+console.log("UPenn &ndash; Penn IUR".replace(/[^A-Z]+/ig, "")); //remove
 
 
 var app = express();
-var scoresFinal = [];
-// take express stuff out of the setInterval, use a standin to fill up scores and 
-// then reassign to scores within setInterval once it's ready to go
+app.listen(3001);
 
-var i = 0;
+nunjucks.configure('views', {
+	autoescape: true,
+	    express   : app
+});
+
+var scoresFinal = [];
+
 setInterval(function () {
 	var scores = [];
 	request({
@@ -23,7 +29,9 @@ setInterval(function () {
 		async.each(sitearray, function(site, callback) {
      		//siteAttributes object has a name, url, status, and (load) secs
      		var siteAttributes = {};
-     		siteAttributes.sname = site.name;
+     		siteAttributes.sname = site.name.replace('&ndash;', '-');
+     		siteAttributes.sname = site.name.replace('&amp;', 'and');
+     		siteAttributes.scode = site.name.replace(/[^A-Z]+/ig, "");
 			//if the final site works
 			if(site.final_production_url) {
 				//clean up the url
@@ -35,24 +43,46 @@ setInterval(function () {
 				request(site.final_production_url, function(err, response, body) {
 					var end = +new Date();
 					if(!err) {
+
 						siteAttributes.secs = (end-start)/1000;
 						siteAttributes.statusCode = response.statusCode;
+						if(response.statusCode != 200)
+							siteAttributes.scolor = '#800000';
+						else {
+							var lightness = 255 - (siteAttributes.secs*12)
+							lightness = Math.round(lightness);
+							if(lightness < 0)
+								lightness = 0;
+							siteAttributes.scolor = rgbToHex(255, lightness, lightness);
+						}
 						scores.push(siteAttributes);
+						//create text to display?
+	//assign to a color of red, corresp. to # of seconds, reversed, and assign other 
+	//val.s to 255 since white is 255,255,255
+    //you can use function 
+						//if response.statusCode isn't 200, assign to maroon
 						callback();
 					}
 					else {
-						siteAttributes.statusCode = -1;
+						//create text to display?
+						siteAttributes.scolor = '#800000';
+						siteAttributes.secs = 5000; 
 						scores.push(siteAttributes);
 						callback();
 					}
 				});
 			}
 			else { 
+				siteAttributes.scolor = '#800080';
+				siteAttributes.secs = -1;
+				//create text to display?
 				siteAttributes.url = 'NO URL LISTED';
 				scores.push(siteAttributes);
 				callback();
 			}
 		}, function(err) {
+			//sort scores by secs before putting into scoresfinal
+			scores.sort(compare); 
 			scoresFinal = scores;
 			console.log("done, scores looks like:   ");
 			scores.forEach(function(siteattr) {
@@ -63,16 +93,25 @@ setInterval(function () {
 }, 1000*2*60);
 
 
-app.get('/', function (req, res) {
-	while(true) {
-		res.send(scoresFinal);
-	}
-});
+function rgbToHex(r, g, b) { 
+	return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b); }
 
-var server = app.listen(3000, function () {
-	var host = server.address().address;
-	var port = server.address().port;
+function componentToHex(c) {
+    var hex = c.toString(16);
+    return hex.length == 1 ? "0" + hex : hex;
+}
 
-	console.log('Example app listening at http://%s:%s', host, port);
-				//console.log("scores are \n \n \n " + scores);
-			});
+function compare(a,b) {
+  if (a.secs < b.secs)
+    return 1;
+  if (a.secs > b.secs)
+    return -1;
+  return 0;
+}
+
+app.get('/', function(req, res) {
+	res.render('index.html', {
+		title : 'Test Page',
+		    items : scoresFinal
+		    });
+    });
--- old/app_defensive_workswithoutfrontend.js
+++ old/app_defensive_workswithoutfrontend.js
@@ -2,21 +2,25 @@ var config = require('./config.js');
 var request = require('request');
 var async = require('async');
 var express = require('express');
+var nunjucks = require('nunjucks');
+
 
 var app = express();
+var scoresFinal = [];
+// take express stuff out of the setInterval, use a standin to fill up scores and 
+// then reassign to scores within setInterval once it's ready to go
 
-function singleRequest() {
+var i = 0;
+setInterval(function () {
 	var scores = [];
-
 	request({
 		uri: config.apiurl,
 		strictSSL: false,
 		jar: true
 	}, 
-	function(err, response, body) {		
-        var sitearray = JSON.parse(body);
-     	//var i = 0; //del
-     	async.each(sitearray, function(site, callback) {
+	function(err, response, body) {
+		var sitearray = JSON.parse(body);
+		async.each(sitearray, function(site, callback) {
      		//siteAttributes object has a name, url, status, and (load) secs
      		var siteAttributes = {};
      		siteAttributes.sname = site.name;
@@ -27,55 +31,43 @@ function singleRequest() {
 					site.final_production_url = 'http://' + site.final_production_url;
 				}
 				siteAttributes.url = site.final_production_url;
-				//console.log(site.final_production_url + " ?= " + siteAttributes.url);
-
 				var start = +new Date();
 				request(site.final_production_url, function(err, response, body) {
 					var end = +new Date();
 					if(!err) {
-						//console.log(site.final_production_url + ": " + response.statusCode);
 						siteAttributes.secs = (end-start)/1000;
 						siteAttributes.statusCode = response.statusCode;
-						//console.log("loaded in " + (end-start)/1000 + " seconds");
-						console.dir(siteAttributes);
 						scores.push(siteAttributes);
-						//console.dir(siteAttributes);
 						callback();
 					}
 					else {
 						siteAttributes.statusCode = -1;
-						console.dir(siteAttributes);
 						scores.push(siteAttributes);
-						//console.dir(siteAttributes);
 						callback();
 					}
 				});
 			}
-			else { //console.log(site.final_production_url + ": " + "error");
-			siteAttributes.url = 'NO URL LISTED';
-			scores.push(siteAttributes);
-			console.dir(siteAttributes);
-			callback();
-		}
-	}, function(err) {
-		// app.delete('/', function (req, res) {
-		//   res.send('DELETE request to homepage');
-		// });
-		console.log("done \n \n \n");
-		app.get('/', function (req, res) {
-			res.send(scores);
-		});
+			else { 
+				siteAttributes.url = 'NO URL LISTED';
+				scores.push(siteAttributes);
+				callback();
+			}
+		}, function(err) {
+			scoresFinal = scores;
+			console.log("done, scores looks like:   ");
+			scores.forEach(function(siteattr) {
+				console.log(siteattr);
+			});
 		})
 });
-}
+}, 1000*2*60);
 
 
-singleRequest();
-
-setInterval(function () {
-	console.log("~~~~~~~~~~~Hello@13~~~~~~~~~~~");
-	singleRequest();
-}, 1000*60*2.5);
+app.get('/', function (req, res) {
+	while(true) {
+		res.send(scoresFinal);
+	}
+});
 
 var server = app.listen(3000, function () {
 	var host = server.address().address;
@@ -83,4 +75,4 @@ var server = app.listen(3000, function () {
 
 	console.log('Example app listening at http://%s:%s', host, port);
 				//console.log("scores are \n \n \n " + scores);
-});
+			});

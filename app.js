bad1d909df8eac123b4487d30b0bbd15bd756888
daphneweinstein <dhaw@princeton.edu>
2015-07-28 16:57:53 -0400

    partially working with averages

--- app.js
+++ app.js
@@ -8,166 +8,284 @@ var nunjucks = require('nunjucks');
 var app = express();
 var mongodb = require('mongodb');
 
+var DAY_IN_MILISECONDS = 86400000;
+
+//set up the server
 app.listen(3001);
 
+//use nunjucks instead of jade
 nunjucks.configure('views', {
 	autoescape: true,
 	express   : app
 });
 
+//set up route for home page
+var scoresFinal = [];
+app.get('/', function(req, res) {
+	res.render('index.html', {
+		title : 'Test Page',
+		items : scoresFinal
+	});
+});
+
+//set up mongo and connect to statuschecks db
 var MongoClient = mongodb.MongoClient;
 var url = 'mongodb://localhost:27017/statuschecks';
-			//settimeout function and make stuff inside a named function as well
-MongoClient.connect(url, function (err, db) {
-	if (err) {
-		console.log('Unable to connect to the mongoDB server. Error:', err);
-	} else {
-    //HURRAY!! We are connected. :)
-	console.log('Connection established to', url);
 
-    // Get the documents collection
-    var collection = db.collection('sites');
+var collection;
+var checksites = [];
 
-	var scoresFinal = [];
+async.series([
+    function(callback){
+    	//connect to mongo
+        MongoClient.connect(url, function (err, db) {
+			if (err) {
+				console.log('Unable to connect to the mongoDB server. Error:', err);
+				callback(err);
+			} else {
+		    	//HURRAY!! We are connected. :)
+				console.log('Connection established to', url);
 
-	
+		    	// Get the documents collection
+		    	collection = db.collection('sites');
 
-	app.get('/', function(req, res) {
-		res.render('index.html', {
-			title : 'Test Page',
-			items : scoresFinal
-		});
-	});
+		    	//clean out all old data 
+		    	var timeRecorded = +new Date();
+				var keepTime = timeRecorded - DAY_IN_MILISECONDS;
+//collection.remove( { time: { $lt: keepTime } } );
+
+		    	callback(null);
+        	}
+        });
+    },
+    function(callback){
+    	//hit darienst and get back cleaned array of sites
+    	request({
+			uri: config.apiurl,
+			strictSSL: false,
+			jar: true
+			}, 
+			function(err, response, body) {
+				//parse the response
+				var sitearray = JSON.parse(body);
+				
+				// collection.insert({name: 'darienst', value: sitearray}, function (err, result) {
+				// 	if (err) {
+				// 		console.log(err);
+				// 	} else {
+				// 		console.log('Inserted sitearray sucessfully');
+				// 	}
+				// });
+
+    			//loop over responses and clean them up
+				sitearray.forEach(function(rawsite) {
+					rawsite.name = rawsite.name.replace('&amp;', 'and');
+					rawsite.name = rawsite.name.replace(/[^A-Z0-9+(). ]+/ig, '-');
+					rawsite.name = rawsite.name.replace('ndash', '');
+					rawsite.code = rawsite.name.replace(/[^A-Z]+/ig, "");
+
+					if(rawsite.final_production_url) {
+						//clean up the url
+						if(rawsite.final_production_url.indexOf("http") == -1) {
+							rawsite.final_production_url = 'http://' + rawsite.final_production_url;
+						}
+						checksites.push(rawsite);
+					} else {
+						console.log('no url listed for this site: ' + rawsite);
+					}
+				});
+
+				callback(null);
+			});
+    }
+],
+// optional callback
+function(err, results){
+	if(err) {
+		console.log(err);
+		return;
+	}
+
+	getValues();
+	setInterval(getValues, 1000*2*60);
+
+});
 
 
-	var checksites = [];
+function getValues(){
+	var scores = [];
+	async.each(checksites, function(site, callback) {
+     	//siteAttributes object has a name, url, status, and (load) secs
+     	var siteAttributes = {};
+     	siteAttributes.sname = site.name;
+     	siteAttributes.scode = site.code;
 
-	
+     	var start = +new Date();
+     	var requested = {url: site.final_production_url, timeout: 5000};
+     	request(requested, function(err, response, body) {
+     		var end = +new Date();
+     		if(!err) {
 
-	request({
-		uri: config.apiurl,
-		strictSSL: false,
-		jar: true
-		}, 
-		function(err, response, body) {
-			var sitearray = JSON.parse(body);
-			
-			collection.insert({name: 'darienst', value: sitearray}, function (err, result) {
+     			siteAttributes.secs = (end-start)/1000;
+     			if(response.statusCode != 200) {
+     				siteAttributes.scolor = '#800000';
+     				siteAttributes.descr = siteAttributes.sname + ' loaded in ' + siteAttributes.secs + 's. with status code ' + response.statusCode;
+     				//manipulate database info
+     			}
+     			else {
+     				var lightness = 255 - (siteAttributes.secs*12)
+     				lightness = Math.round(lightness);
+     				if(lightness < 0)
+     					lightness = 0;
+     				siteAttributes.scolor = rgbToHex(255, lightness, lightness);
+     				siteAttributes.descr = siteAttributes.sname + ' loaded in ' + siteAttributes.secs + 's.';
+     			}     			
+     		}
+     		else {
+     			siteAttributes.scolor = '#800000';
+     			siteAttributes.secs = 5000; 
+     			siteAttributes.descr = siteAttributes.sname + ' -- load error';
+     			
+     		}
+
+     		//store all data as object with a timestamp, scode (name of site), and time to load
+     		var dbEntry = { scode: siteAttributes.scode, time: start, timeToLoad: siteAttributes.secs  };
+
+     		//insert database entry
+     		collection.insert(dbEntry, function (err, result) {
 				if (err) {
 					console.log(err);
 				} else {
-					console.log('Inserted sitearray sucessfully');
+					//console.log('Inserted databaseEntry sucessfully');
 				}
 			});
 
-			sitearray.forEach(function(rawsite) {
-				rawsite.name = rawsite.name.replace('&amp;', 'and');
-				rawsite.name = rawsite.name.replace(/[^A-Z0-9+(). ]+/ig, '-');
-				rawsite.name = rawsite.name.replace('ndash', '');
-				rawsite.code = rawsite.name.replace(/[^A-Z]+/ig, "");
+     		//get shortest load time
+     		var cursor = collection.find({scode: siteAttributes.scode });
+     		cursor.sort({timeToLoad: 1}); 
+     		cursor.limit(1);
+     		cursor.toArray(function (err, result) {
+			    if (err) {
+			        console.log(err);
+			    } else if (result.length) {
+			        // console.log('Found:', result);
+			        siteAttributes.shortest = result.timeToLoad;
+			        //do work if we want average
+			    } else {
+			        console.log('No document(s) found with defined "find" criteria!');
+			    }
+		    });
 
-				if(rawsite.final_production_url) {
-					//clean up the url
-					if(rawsite.final_production_url.indexOf("http") == -1) {
-						rawsite.final_production_url = 'http://' + rawsite.final_production_url;
-					}
-				checksites.push(rawsite);
-				}
-				else {
-					console.log('no url listed for this site: ' + rawsite);
-				}
-			});
+     		// //get longest load time
+     		var cursor = collection.find({scode: siteAttributes.scode });
+     		cursor.sort({timeToLoad: -1}); 
+     		cursor.limit(1);
+     		cursor.toArray(function (err, result) {
+			    if (err) {
+			        console.log(err);
+			    } else if (result.length) {
+			      //   console.log('Found:', result);
+			     	// console.log('\n \n \n');
+			        siteAttributes.longest = result.timeToLoad;
+			        //do work if we want average
+			    } else {
+			        console.log('No document(s) found with defined "find" criteria!');
+			    }
+		    });
 
-			console.log('line 81');
-			getValues(checksites);
-			setInterval(run, 1000*2*60);
+
+
+
+     		//calculate total average
+     		var averageObj = collection.aggregate(
+			   [
+			   	{ $match: { scode: siteAttributes.scode } },
+			     {
+			       
+			       $group:
+			         {
+			           _id: "$scode",
+			           fullAvg: { $avg: "$timeToLoad" } 
+			         }
+			     }
+			   ]
+			).toArray(function (err, result) {
+			    if (err) {
+			        console.log(err);
+			    } else if (result.length) {
+
+			        //console.log('Found full average? : ', result[0].fullAvg);
+			     	// console.log('\n \n \n');
+			        siteAttributes.avg = result[0].fullAvg;
+			        //do work if we want average
+			    } else {
+			        console.log('No document(s) found with defined "find" criteria!');
+			    }
+		    });
+
+
+     		//calculate last 4 average
+     		var cursor = collection.find({scode: siteAttributes.scode });
+     		cursor.sort({time: -1}); 
+     		cursor.limit(4);
+     		cursor.toArray(function (err, result) {
+			    if (err) {
+			        console.log(err);
+			    } else if (result.length) {
+					//calculate average of most recent 4 loads
+			     	var lastFour = 0;
+			     	result.forEach(function(res) {
+			     		lastFour = lastFour + res.timeToLoad;
+			     	});
+			     	lastFour = lastFour/4;
+
+			     	//alert true if the recent load times are bad
+			     	if(lastFour > 2*siteAttributes.avg) {
+			        	siteAttributes.alert = true;
+			        }
+			        else siteAttributes.alert = false;
+			    } else {
+			        console.log('No document(s) found with defined "find" criteria!');
+			    }
+		    });
+
+//TEMPORARY PATCH update the description
+			console.log(siteAttributes.avg);
+     		siteAttributes.descr = siteAttributes.descr + " average: " + siteAttributes.avg 
+     		+ " lowest: " + siteAttributes.shortest + " highest: " + siteAttributes.longest;
+
+     		
+     		callback();
+     	});
+
+				
+	}, function(err) {
+		timeRecorded = +new Date();
+
+		scores.sort(compare); 
+		scoresFinal = scores;
+		
+		keepTime = timeRecorded - DAY_IN_MILISECONDS;
+
+		//remove old times as we go along
+		collection.remove( { time: { $lt: keepTime } } );
 
 	});
-
-	function run() {
-    	getValues(checksites);
-	}
-
+}
 	
 
-	function getValues(sitearray){
-		var scores = [];
-		async.each(sitearray, function(site, callback) {
-	     		//siteAttributes object has a name, url, status, and (load) secs
-	     		var siteAttributes = {};
-	     		
-	     		// siteAttributes.sname = site.name.replace('&amp;', 'and');
-	     		// siteAttributes.sname = siteAttributes.sname.replace(/[^A-Z0-9+(). ]+/ig, '-');
-	     		// siteAttributes.sname = siteAttributes.sname.replace('ndash', '');
-	     		// siteAttributes.scode = site.name.replace(/[^A-Z]+/ig, "");
-				siteAttributes.sname = site.name;
-				siteAttributes.scode = site.code;
-					//siteAttributes.url = site.final_production_url;
-					var start = +new Date();
-					var requested = {url: site.final_production_url, timeout: 5000};
-					request(requested, function(err, response, body) {
-						var end = +new Date();
-						if(!err) {
-
-							siteAttributes.secs = (end-start)/1000;
-							if(response.statusCode != 200) {
-								siteAttributes.scolor = '#800000';
-								siteAttributes.descr = siteAttributes.sname + ' loaded in ' + siteAttributes.secs + 's. with status code ' + response.statusCode;
-							}
-							else {
-								var lightness = 255 - (siteAttributes.secs*12)
-								lightness = Math.round(lightness);
-								if(lightness < 0)
-									lightness = 0;
-								siteAttributes.scolor = rgbToHex(255, lightness, lightness);
-								siteAttributes.descr = siteAttributes.sname + ' loaded in ' + siteAttributes.secs + 's.';
-							}
-							scores.push(siteAttributes);
-							callback();
-						}
-						else {
-							siteAttributes.scolor = '#800000';
-							siteAttributes.secs = 5000; 
-							siteAttributes.descr = siteAttributes.sname + ' -- load error';
-							scores.push(siteAttributes);
-							callback();
-						}
-					});
-				
-				
-		}, function(err) {
-			var timeRecorded = +new Date();
-
-			scores.sort(compare); 
-			scoresFinal = scores;
-						//db.collection.find().skip(db.collection.count() - N) 
-						//^^ this is the stupid approach and doesnt ever delete data
-						//it appears in: http://stackoverflow.com/questions/4421207/mongodb-how-to-get-the-last-n-records
-						//probably better since i am timestamping to just delete entries that are old
-						
 
 
 
-						//manipulate collection, probably using a for loop to go through and look for big diffs
-						//also probably easiest to recalculate averages every time as opposed to trying to reverse-engineer
-						var databaseEntry = { time: timeRecorded, scores: scoresFinal };
-						console.log("line 119");
-						collection.insert(databaseEntry, function (err, result) {
-							if (err) {
-								console.log(err);
-							} else {
-								console.log('Inserted databaseEntry sucessfully');
-							}
-						});
-						//
-						console.log("done, scores looks like:   ");
-						scores.forEach(function(siteattr) {
-							console.log(siteattr);
-						});
-					});
-		}
-	}
-});
+
+
+
+
+
+
+
+
+
 
 
 
